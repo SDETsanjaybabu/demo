392f9439d80bb67c3983f3270abeb6f95bc87cdc
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-05-03 17:30:24 +0530

    1st commit

---
+++ tyss/src/test/java/sample/UnitTest.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class UnitTest {
+	@Test
+	public void hi()
+	{
+		System.out.println("hi guys");
+	}
+	@Test
+	public void hello()
+	{
+		System.out.println("hello guys");
+	}
+
+}
